0c609519162815f4e79c61a86f584fe815f012f6
Natural-Minecraft <naturalsmp.mc@gmail.com>
2026-02-07 22:48:13 +0700

    fix: explicit shadowJar configuration to ensure NaturalModels.jar is in the correct path

--- platform/paper/build.gradle.kts
+++ platform/paper/build.gradle.kts
@@ -36,6 +36,9 @@ val generatePaperLibrary by tasks.registering {
 
 tasks.shadowJar {
     archiveFileName.set("NaturalModels.jar")
+    archiveBaseName.set("NaturalModels")
+    archiveClassifier.set("")
+    archiveVersion.set("")
     destinationDirectory.set(layout.buildDirectory.dir("libs"))
     dependsOn(generatePaperLibrary)
     from(libraryDir)
